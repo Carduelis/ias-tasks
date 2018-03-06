29b4f5db87dfb6132c5f0a92cfc45798f52f8de0
Carduelis <pavepy@gmail.com>
2018-03-06 10:58:33 +0300

    UI-kit, Label component.

---
+++ app/components/UI/Label.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
+
+class Label extends Component {
+	render() {
+		const { type = 'default', label, value } = this.props;
+		return (
+			<div className={`label label-${type}`}>
+				<span className="label-text">
+					{label}
+				</span>
+				<span className="label-value">
+					{value}
+				</span>
+			</div>
+		);
+	}
+}
+
+const numberOrAString = PropTypes.oneOfType([
+	PropTypes.string,
+	PropTypes.number
+]);
+Label.propTypes = {
+	label: numberOrAString.isRequired,
+	value: numberOrAString.isRequired,
+	type: PropTypes.oneOf(['error', 'success', 'warning', 'info', 'default'])
+};
+
+export { Label };
